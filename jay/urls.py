4858fec429f053c95f9b3805cd34237128acf49e
Ernest <ieifeanyichukwu@gmail.com>
2021-02-10 16:34:28 +0100

    Update urls.py

--- jay/urls.py
+++ jay/urls.py
@@ -23,6 +23,3 @@ urlpatterns = [
 urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Admin username and password
-# ernestizic - 30094210cd
